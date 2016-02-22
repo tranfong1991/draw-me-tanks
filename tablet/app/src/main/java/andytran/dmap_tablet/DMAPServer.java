b4443db097c40f6ad74b7c0d5edb4736642adb9c
Andy Tran <tran_fong1991@yahoo.com>
2016-02-22 14:00:38 -0600

    working token

--- tablet/app/src/main/java/andytran/dmap_tablet/DMAPServer.java
+++ tablet/app/src/main/java/andytran/dmap_tablet/DMAPServer.java
@@ -2,10 +2,13 @@ package andytran.dmap_tablet;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
 
 import java.io.IOException;
 import java.util.Map;
+import java.util.Random;
 
 import fi.iki.elonen.NanoHTTPD;
 
@@ -20,43 +23,54 @@ import fi.iki.elonen.NanoHTTPD;
  * 4. In case the route returns a JSON object, simply add quotes around keys
  */
 public class DMAPServer extends NanoHTTPD {
+    private static final String ALPHABET = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private static final int TOKEN_LENGTH = 10;
+
+    public static final String PREF_NAME = "DMAP_PREF";
+    public static final String PREF_TOKEN = "DMAP_TOKEN";
     public static final int PORT = 8080;
     public static final int HTTP_OK = 200;
     public static final int HTTP_UNAUTHORIZED = 401;
+    public static final int HTTP_NOT_FOUND = 404;
     public static final String PACKAGE_NAME = "andytran.dmap_tablet";
     public static final String EXTRA_GRAPHIC_ID = "EXTRA_GRAPHIC_ID";
     public static final String EXTRA_ACTION = "EXTRA_ACTION";
 
-    private static String TOKEN = "ABC123";
-
+    private String token;
     private Context context;
 
     public DMAPServer(Context context)throws IOException{
         super(PORT);
 
+        SharedPreferences pref = context.getSharedPreferences(PREF_NAME, 0);
+        this.token = pref.getString(PREF_TOKEN, null);
         this.context = context;
+
         start();
     }
 
     @Override
     public Response serve(IHTTPSession session) {
         Map<String, String> params = session.getParms();
-
-        //check if token is not present or doesn't match
-        if(params.get("token") == null || !params.get("token").equals(TOKEN))
-            return newFixedLengthResponse("{\"status\":" + HTTP_UNAUTHORIZED + "}");
-
         String uri = session.getUri();
         String method = session.getMethod().name();
 
+        //when the phone first connects to the tablet
+        if(method.equals("GET") && uri.equals("/generate"))
+            return generateToken();
+
+        //check if token is not present or doesn't match
+        if(params.get("token") == null || !params.get("token").equals(token))
+            return newFixedLengthResponse("{\"status\":" + HTTP_UNAUTHORIZED + "}");
+
         if(method.equals("GET")) {
             switch(uri){
-                case "/generate":
-                    return generateToken();
                 case "/play":
                     return playGraphic(params);
                 case "/stop":
                     return stopGraphic();
+                case "/deactivate":
+                    return deactivateToken();
             }
         } else if(method.equals("POST")){
             switch(uri){
@@ -67,13 +81,35 @@ public class DMAPServer extends NanoHTTPD {
             switch(uri){
                 case "/graphic":
                     return deleteGraphic(params);
+                case "/deactivate":
+                    return deactivateToken();
             }
         }
-        return newFixedLengthResponse("Nothing");
+        return newFixedLengthResponse("{\"status\":" + HTTP_NOT_FOUND + "}");
     }
 
     private Response generateToken(){
-        return null;
+        //device already has a token
+        if(token != null)
+            return newFixedLengthResponse("{\"status\":" + HTTP_UNAUTHORIZED + "}");
+
+        StringBuffer buffer = new StringBuffer();
+        Random rand = new Random();
+
+        //randomly generate a token
+        for(int i = 0; i<TOKEN_LENGTH; i++){
+            char c = ALPHABET.charAt(rand.nextInt(ALPHABET.length()));
+            buffer.append(c);
+        }
+
+        SharedPreferences pref = context.getSharedPreferences(PREF_NAME, 0);
+        SharedPreferences.Editor editor = pref.edit();
+        editor.putString(PREF_TOKEN, buffer.toString());
+        editor.commit();
+
+        this.token = buffer.toString();
+
+        return newFixedLengthResponse("{\"token\" : \"" + buffer.toString() + "\"}");
     }
 
     private Response postGraphic(Map<String, String> params){
@@ -99,6 +135,20 @@ public class DMAPServer extends NanoHTTPD {
         Intent intent = new Intent(PACKAGE_NAME);
         intent.putExtra(EXTRA_ACTION, "stop");
         LocalBroadcastManager.getInstance(context).sendBroadcast(intent);
+
+        return newFixedLengthResponse("{\"status\" : " + HTTP_OK + "}");
+    }
+
+    private Response deactivateToken(){
+        if(token == null)
+            return newFixedLengthResponse("{\"status\":" + HTTP_NOT_FOUND + "}");
+
+        SharedPreferences pref = context.getSharedPreferences(PREF_NAME, 0);
+        SharedPreferences.Editor editor = pref.edit();
+        editor.putString(PREF_TOKEN, null);
+        editor.commit();
+
+        this.token = null;
 
         return newFixedLengthResponse("{\"status\" : " + HTTP_OK + "}");
     }
