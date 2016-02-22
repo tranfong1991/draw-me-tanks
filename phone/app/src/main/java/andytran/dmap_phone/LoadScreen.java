1bbdb36695c4f00618bdfe240431267a70b7bd77
Karrie Cheng <karriecheng@tamu.edu>
2016-02-21 22:37:09 -0600

    Add phone and tablet loading screens
    Buttons lead to placeholder menu page.
    Needs to handle sockets.

---
+++ phone/app/src/main/java/andytran/dmap_phone/LoadScreen.java
@@ -0,0 +1,60 @@
+package andytran.dmap_phone;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+
+public class LoadScreen extends AppCompatActivity {
+    public String ipAddress = "10.0.2.15";
+    public int port = 8080;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_load_screen);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_load_screen, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void submitData(View view){
+//
+//        Intent intent = new Intent(this, HamActivity.class);
+//        Button buttonText = (Button) findViewById(R.id.button);
+//        String message = buttonText.getText().toString();
+//        intent.putExtra(EXTRA_MESSAGE, message);
+//        startActivity(intent);
+
+        Intent intent = new Intent(this, MainScreen.class);
+        EditText ipAddressText = (EditText)findViewById(R.id.ip_field);
+        EditText portNumberText  = (EditText)findViewById(R.id.port_field);
+        ipAddress = ipAddressText.getText().toString();
+        port = Integer.parseInt(portNumberText.getText().toString());
+
+        startActivity(intent);
+
+
+    }
+}
