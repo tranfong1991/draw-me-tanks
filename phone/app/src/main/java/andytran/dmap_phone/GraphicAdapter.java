80374850e50a6d38826ae34d2afc9766b30750fd
Shane Scott <Shane Scott>
2016-03-29 18:10:12 -0500

    ListView swipe working

--- phone/app/src/main/java/andytran/dmap_phone/GraphicAdapter.java
+++ phone/app/src/main/java/andytran/dmap_phone/GraphicAdapter.java
@@ -8,37 +8,89 @@ import android.widget.ArrayAdapter;
 import android.widget.TextView;
 import android.widget.ImageView;
 
-class GraphicAdapter extends ArrayAdapter<String> {
+import com.daimajia.swipe.SwipeLayout;
+import com.daimajia.swipe.adapters.ArraySwipeAdapter;
+
+class GraphicAdapter extends ArraySwipeAdapter<String> {
     private final Activity context;
     private final String[] arr;
     private final Integer[] imgid;
+    private SwipeLayout swipeLayout;
 //    Context c;
 //    String[] items;
 //    int [] images;
 //    int layoutResourceId;
 //    LayoutInflater inflater;
-
+    @Override
+    public int getSwipeLayoutResourceId(int position) {
+        return R.id.sample1;
+    }
     public GraphicAdapter(Activity context, String[] arr, Integer[] imgid) {
         super(context, R.layout.graphic_item, arr);
        // inflater = (LayoutInflater) c.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         this.context = context;
         this.arr = arr;
         this.imgid = imgid;
+
+
+
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         LayoutInflater inflater=context.getLayoutInflater();
-        View rowView=inflater.inflate(R.layout.graphic_item, null, true);
 
-        TextView textView = (TextView) rowView.findViewById(R.id.textView1);
-        ImageView imageView1 = (ImageView) rowView.findViewById(R.id.imageView1);
-        ImageView imageView2 = (ImageView) rowView.findViewById(R.id.imageView2);
+        swipeLayout =  (SwipeLayout) inflater.inflate(R.layout.graphic_item, null, true);
+        swipeLayout.setShowMode(SwipeLayout.ShowMode.PullOut);
 
+//set show mode.
+        swipeLayout.setShowMode(SwipeLayout.ShowMode.LayDown);
+
+
+//add drag edge.(If the BottomView has 'layout_gravity' attribute, this line is unnecessary)
+        swipeLayout.addDrag(SwipeLayout.DragEdge.Left, swipeLayout.findViewById(R.id.bottom_wrapper));
+
+        swipeLayout.addSwipeListener(new SwipeLayout.SwipeListener() {
+            @Override
+            public void onClose(SwipeLayout layout) {
+                //when the SurfaceView totally cover the BottomView.
+            }
+
+            @Override
+            public void onUpdate(SwipeLayout layout, int leftOffset, int topOffset) {
+                //you are swiping.
+            }
+
+            @Override
+            public void onStartOpen(SwipeLayout layout) {
+
+            }
+
+            @Override
+            public void onOpen(SwipeLayout layout) {
+                //when the BottomView totally show.
+            }
+
+            @Override
+            public void onStartClose(SwipeLayout layout) {
+
+            }
+
+            @Override
+            public void onHandRelease(SwipeLayout layout, float xvel, float yvel) {
+                //when user's hand released.
+            }
+        });
+
+        TextView textView = (TextView) swipeLayout.findViewById(R.id.textView1);
+        ImageView imageView1 = (ImageView) swipeLayout.findViewById(R.id.imageView1);
+        ImageView imageView2 = (ImageView) swipeLayout.findViewById(R.id.imageView2);
 
         textView.setText(arr[position]);
         imageView1.setImageResource(imgid[position]);
-        return rowView;
+        return swipeLayout;
+
+
 
 
         //View customView = inflater.inflate(R.layout.graphic_item, parent, false);
--- phone/app/src/main/java/andytran/dmap_phone/MainActivity.java
+++ phone/app/src/main/java/andytran/dmap_phone/MainActivity.java
@@ -10,21 +10,35 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 
+import com.daimajia.swipe.SwipeLayout;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        list = (ListView)findViewById(R.id.listView);
+        //Log.v("LstAdapter", "Inside LstAdapter");
+        GraphicAdapter adapter = new GraphicAdapter(this,items, imgid);
+        list.setAdapter(adapter);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//        ListView listView = (ListView) findViewById(R.id.listView); this is the code that works
+//        ArrayList<String> itemList = new ArrayList<String>();
+//        itemList.addAll(Arrays.asList(items));
+//        lsAdapter = new ArrayAdapter<String>(this,R.layout.graphic_item, R.id.textView1, items);  this is the code that works
+//        listView.setAdapter(lsAdapter);
+
+        //populateListView();
+
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
     }
 
 }
