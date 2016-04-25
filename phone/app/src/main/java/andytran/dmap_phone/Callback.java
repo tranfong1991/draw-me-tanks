b0d0df4717228968e34bc7cf2620029e35bd8b07
Andy Tran <tran_fong1991@yahoo.com>
2016-04-25 00:17:40 -0500

    deleted Callback class

--- phone/app/src/main/java/andytran/dmap_phone/Callback.java
+++
@@ -1,8 +0,0 @@
-package andytran.dmap_phone;
-
-/**
- * Created by Andy Tran on 4/24/2016.
- */
-public abstract class Callback {
-    public abstract void run();
-}
--- phone/app/src/main/java/timothy/dmap_phone/InstructionalGraphicTimer.java
+++ phone/app/src/main/java/timothy/dmap_phone/InstructionalGraphicTimer.java
@@ -1,14 +1,12 @@
 package timothy.dmap_phone;
 
 import android.content.Context;
-import android.util.Log;
 
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 
 import java.util.HashMap;
-import java.util.Map;
 import java.util.Timer;
 import java.util.TimerTask;
 
