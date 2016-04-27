b12ddb1f74b398d9e03ffe38262b1298c6da842a
Shane Scott <Shane Scott>
2016-04-26 23:27:37 -0500

    commit

---
+++ phone/app/src/main/java/andytran/dmap_phone/IpAccess.java
@@ -0,0 +1,13 @@
+package andytran.dmap_phone;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class IpAccess extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ip_access);
+    }
+}
